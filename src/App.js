67fdd2d3985906bd7a0d4e0e30a5d39f21ca1f9f
Devin Blankenship <failure2fly@gmail.com>
2020-04-28 18:55:00 -0700

    Added react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <Row>
+          <Col xs={3}>2 of 3 (wider)</Col>
+        </Row>
+        <Row>
+          <Col xs={9}>2 of 3 (wider)</Col>
+        </Row>
+      </Container>
+      
     </div>
   );
 }
