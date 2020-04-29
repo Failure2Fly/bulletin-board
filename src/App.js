952d8ce3cf4d33ba8352a8f731bd8bd134c286e4
Devin Blankenship <failure2fly@gmail.com>
2020-04-28 19:11:38 -0700

    created new branch

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import './App.css';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 function App() {
   return (
-    <div className="App">
-      <Container>
-        <Row>
+    <div >
+      <Container className="Nav-Container">
+        <Row className="Nav-Row">
           <Col xs={3}>2 of 3 (wider)</Col>
         </Row>
-        <Row>
+        <Row className="Nav-Row">
           <Col xs={9}>2 of 3 (wider)</Col>
         </Row>
       </Container>
