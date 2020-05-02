1969ba10fb93d050b31d7aec2e0f8e22df395acc
jlweis2 <jlweis2@mail.fhsu.edu>
2020-05-01 20:29:09 -0700

    started new post

--- src/Bulletin.js
+++ src/Bulletin.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './Bulletin.css';
-
-
+import NewPostInput from './NewPostInput';
 function Bulletin() {
   return (
     <div className="bulletin col col-lg-10">
-        
+    <NewPostInput />
+    
     </div>
   );
 }
